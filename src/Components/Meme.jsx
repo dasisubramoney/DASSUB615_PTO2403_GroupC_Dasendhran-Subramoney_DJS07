3494b08612bdcddbd9bcbcb651f4fd41a47d794b
Dasendhran Subramoney <das.subramoney@gmail.com>
2025-01-26 16:49:41 +0200

    Added the async function in our meme component

--- src/Components/Meme.jsx
+++ src/Components/Meme.jsx
@@ -2,12 +2,21 @@ import React from "react"
 
 function Meme() {
 
+    const [meme, setMeme] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg" 
+    })
+
     const [allMemes, setAllMemes] = React.useState(memesData)
 
     React.useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-            .then(res => res.json())
-            .then(data => setAllMemes(data.data.memes))
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMemes(data.data.memes)
+        }
+        getMemes()
     }, [])
     
     function getMemeImage() {
@@ -34,7 +43,7 @@ function Meme() {
             <div className="form">
             <div>
                 <input
-                    ype="text"
+                    type="text"
                     placeholder="Top text"
                     className="form--input"
                     name="topText"
